56c77eb1d5a209ebfb3eddaba1edf70ef4be1426
slaweksn <slaweksnieg@gmail.com>
2023-01-19 13:05:01 +0100

    Initial commit

---
+++ src/main/java/org/sx/zadanie/githubrest/GithubRestApplication.java
@@ -0,0 +1,13 @@
+package org.sx.zadanie.githubrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sx/zadanie/githubrest/GithubRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sx.zadanie.githubrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
